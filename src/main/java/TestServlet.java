b44919a3da53ae68c40e502f798171a61411e5ee
Kaizer22 <dshebut@rambler.ru>
2020-02-22 17:38:22 +0300

    Servlet4

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -7,8 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet (name = "test", urlPatterns = "/test")
-
+@WebServlet (name = "test",  urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
