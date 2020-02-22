0980041e18fafb82a465aa57b41d9f4143dbe95e
Kaizer22 <dshebut@rambler.ru>
2020-02-22 15:54:08 +0300

    Correct App Servlet Test

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter pw = resp.getWriter();
+
+        Integer a = 0, b = 0, c = 0;
+        Boolean Error = false;
+
+        String param_a = req.getParameter("a");
+        String param_b = req.getParameter("b");
+
+        try {
+            a = Integer.parseInt(param_a);
+            b = Integer.parseInt(param_b);
+        }
+        catch (NumberFormatException e) {
+            Error = true;
+        }
+
+        if (Error) {
+            pw.println("error");
+        }
+        else {
+            c = a + b;
+
+            pw.println(c);
+        }
+
+    }
+}
